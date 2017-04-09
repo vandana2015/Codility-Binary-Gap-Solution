9ff688113ddadb44c99f4382db41bf98d8edf3a5
vandana2015 <vandanasingh326@gmail.com>
2017-04-09 22:24:35 +0530

    Add files via upload
    The file contains a simple solution to the Codility's 'Binary Gap' problem which gave a score of 100%. The problem is as follows:
    
    A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
    
    For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
    
    Write a function:
    
    class Solution { public int solution(int N); }
    
    that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
    
    For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its

---
+++ BinaryGapSoln.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int N)
+    {
+        if (N == 0) return 0;
+        var bin = Convert.ToString(N, 2);
+        char[] arr = bin.ToCharArray();
+        int count = 0;
+        string result = String.Empty;
+        foreach (char c in arr)
+        {
+            if (c == '0')
+            {
+                count += 1;
+                continue;
+            }
+            if (c == '1')
+            {
+                result = result + "," + count;
+                count = 0;
+            }
+        }
+        result = result.Substring(1, result.Length - 1);
+        int[] arrOfInt = result.Split(',').Select(str => int.Parse(str)).ToArray();
+        //Console.Write(result);
+        return arrOfInt.Max();
+    }
+}
